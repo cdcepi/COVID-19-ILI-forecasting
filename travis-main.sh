2d469af1730ffbfee3075621228ffc74b83b9f3d
Katie House <katiehouse3@gmail.com>
2020-03-16 16:25:06 -0400

    remove deploy keys

--- travis-main.sh
+++ travis-main.sh
@@ -26,15 +26,6 @@ SHA=`git rev-parse --verify HEAD`
 git config user.name "CI auto deploy"
 git config user.email "katiehouse3@gmail.com"
 
-# Setup deploy keys
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
 
 # Deleting the private key so that we don't accidentally push it back
 rm deploy_key
