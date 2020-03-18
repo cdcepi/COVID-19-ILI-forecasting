af63e05cb9f86378df0c8060bf69afc59d3f836b
Katie House <katiehouse3@gmail.com>
2020-03-18 10:51:20 -0400

    Travis run national regional tests

--- code/test-covid-forecast-formatting.R
+++ code/test-covid-forecast-formatting.R
@@ -2,16 +2,23 @@ library(cdcForecastUtils)
 library(dplyr)
 
 # For national/Regional-level forecasts
-weekly_forecast_filepath1 <- "./templates-and-data/covid19-ili-forecast-national-regional-template.csv"
-verify_entry_file(weekly_forecast_filepath1)
+files <- list.files(path="./nation-region-forecast-data", pattern="*.csv", full.names=TRUE, recursive=TRUE)
+
+for(i in files){
+    text <- paste("Testing", i, "...")
+    print(text)
+    verify_entry_file(i)
+}
+
+
 # For state-level forecasts
 # weekly_forecast_filepath2 <- "/directory/EW10-2020-state_forecast_file.csv"
 # verify_entry_file(weekly_forecast_filepath2, challenge = "state_ili")
 
 
 # For national/Regional-level forecasts
-#weekly_forecast_file1 <- read_entry( "./nation-region-forecast-data/UMassCoE-arimaT/metadata-UMassCoE-arimaT.txt")
-#verify_entry(weekly_forecast_file1)
+# weekly_forecast_file1 <- read_entry( "./nation-region-forecast-data/UMassCoE-arimaT/metadata-UMassCoE-arimaT.txt")
+# verify_entry(weekly_forecast_file1)
 # For state-level forecasts
 # weekly_forecast_file2 <- read_entry("/directory/EW10-2020-state_forecast_file.csv")
 # verify_entry(weekly_forecast_file2)
